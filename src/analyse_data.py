ce0b0a4398a1b92e987ae7210ef0a396522a13d3
pa5ca1 <pascal.bock@me.com>
2023-06-08 13:36:28 +0200

    Minor changes in adding labels

--- src/analyse_data.py
+++ src/analyse_data.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_histogram_delta_t(df):
@@ -11,7 +12,7 @@ def plot_histogram_delta_t(df):
     plt.ylabel('log. number of value')
     plt.show()
 
-def adding_meassurement_label(df,n_datapoints):
+def adding_measurement_label(df,n_datapoints):
     '''Adds a label for each meassurement if a) a sample is taken or b) the traverse is moving.
     The decision is based only on the time stamp.
     '''
@@ -23,10 +24,12 @@ def adding_meassurement_label(df,n_datapoints):
     # sampling time.
     # Example: [t_0, t_1, t_2, t_3, ..., t_n] with t_1 < t_2 < t_3 ... t_n
     # and t_n = end_time
-    list_periods = [start_time+i*t_per_datapoint for i in range(n_datapoints+1)]
+    list_periods = np.array([start_time+i*t_per_datapoint for i in range(n_datapoints+1)])
     # We use .squezze to change from pandas dataframe to timeseries
     # because 'get_label' needs a timeseries
     df['label'] = df['Profiles_HostTime_start'].squeeze().apply(get_label,l=list_periods)
+
+    return df
 
 
 def get_label(timestamp,**kwargs):
@@ -37,4 +40,8 @@ def get_label(timestamp,**kwargs):
     for i in range(len(label_list)-1):
         if label_list[i] <= timestamp < label_list[i+1]:
             return chr(65 + i)  # Convert to corresponding label ('A', 'B', 'C', ...)
+    return 'Test'
+
+
+def calculate_mean_per_measurement(df):
     return None
